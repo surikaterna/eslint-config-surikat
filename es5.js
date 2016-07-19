d9a132c961ffb969f0330d31a94429b99b442824
Thomas <tbruun@gmail.com>
2016-07-19 16:16:15 +0200

    initial

---
+++ es5.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: 'airbnb/legacy',
+  rules: {
+    'func-names': 0,
+    'no-multi-spaces': [1, { exceptions: { VariableDeclarator: true } }],
+    'vars-on-top': 0,
+	'comma-dangle': ['error', 'never'],
+    'max-len': ['error', 160, 2]
+  },
+  globals: {
+    describe: true,
+    beforeEach: true,
+    it: true,
+    be: true
+  },
+  env: {
+    node: true,
+    mocha: true
+  }
+};
