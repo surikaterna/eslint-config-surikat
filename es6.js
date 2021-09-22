35c0183249aa9e364a838f1764941555885c3716
Thomas Bruun <tbruun@gmail.com>
2021-09-22 08:19:33 +0200

    update overrides

--- es6.js
+++ es6.js
@@ -1,7 +1,17 @@
 module.exports = {
   extends: 'airbnb/base',
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   rules: {
+    'no-continue': 'off',
+    'object-curly-newline': 'off',
+    'lines-between-class-members': 'off',
+    'no-underscore-dangle': 'off',
+    'prefer-object-spread': 'off',
+    'no-plusplus': 'off',
+    'import/no-unresolved': 'off',
+    'linebreak-style': 'off',
+    'import/extensions': 'off',
+    'class-methods-use-this': 'off',
     'func-names': 0,
     'no-multi-spaces': [1, { exceptions: { VariableDeclarator: true } }],
     'vars-on-top': 0,
