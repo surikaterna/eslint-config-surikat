d4027c4562de0f44d0fd05e53357f8277e60870d
Kenny Ek <ekkenny@hotmail.com>
2022-07-11 09:57:12 +0200

    Replace legacy config with one TS enabled config
    Add config extending sane defaults from airbnb and recommended configs.
    
    Add our own rules based from the es6 config, with TS replacement where necessary.
    
    Old versions use the es6 or es5 configs. New projects should use TS, so we should not need to support separate configs. The TS config works for JS files as well.
    
    Put the config in `index.js` instead of having an unnecessary module just to be exported as default.
    
    Breaking change: Will REQUIRE a `tsconfig.json` to be present in the root of the project.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-module.exports = require('./es6')
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jest/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  parser: '@typescript-eslint/parser',
+  rules: {
+    '@typescript-eslint/comma-dangle': ['error', 'never'],
+    'class-methods-use-this': 'off',
+    'func-names': 'off',
+    'linebreak-style': 'off',
+    'lines-between-class-members': 'off',
+    'max-len': ['error', {
+      code: 160,
+      tabWidth: 2
+    }],
+    'no-continue': 'off',
+    'no-multi-spaces': ['warn', { exceptions: { VariableDeclarator: true } }],
+    'no-plusplus': 'off',
+    'no-underscore-dangle': 'off',
+    'object-curly-newline': 'off',
+    'prefer-destructuring': 'off',
+    'prefer-object-spread': 'off',
+  },
+  env: {
+    node: true,
+    browser: true,
+    jest: true,
+    mocha: true
+  }
+}
